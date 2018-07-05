0631bba571451dd52b2b148de2d84bd48218c3f8
Richard Parkins <richard@saucar.co.uk>
2018-07-05 22:16:24 +0100

    Separate the consumer and service tests

--- tests.sh
+++ tests.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
 
+echo Starting to test the consumer
+
 ./_tests-consumer.sh
+
+echo Finishing testing the consumer
+
+echo Uploading to broker
 
 ./_upload_to_broker.sh
 
+echo Finished uploading to broker
+
+echo Starting service tests against PACT
+
 ./_tests-service.sh
 
+echo Complete
